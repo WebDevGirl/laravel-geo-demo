b7e395b0a4a487138fd38904a778c716153da7a9
Ursula Messick <ursula.messick@usaepay.com>
2017-03-12 10:45:16 -0700

    Add Logo

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,8 +1,12 @@
-<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded fixed-top">
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand" href="#">{{ config('app.name', 'Title') }}</a>
+  
+  <a class="navbar-brand" href="#">
+  	<img src='{{ asset('images/logo.png')}}' width="30" height="30" class="d-inline-block align-top" alt="Logo"> 
+  	{{ config('app.name', 'Title') }}
+  </a>
 
   <div class="collapse navbar-collapse" id="navbarsExampleDefault">
     <ul class="navbar-nav mr-auto">
