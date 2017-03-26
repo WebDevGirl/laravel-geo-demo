3372d29f4cc90d86ddcc8a491e5c70474860ed35
Ursula Messick <ursula.messick@usaepay.com>
2017-03-26 10:55:54 -0700

    Add Tagline to User

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,6 +19,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
+        'tagline' => $faker->sentences($nb = rand(1,2), $asText = true),
         'remember_token' => str_random(10),
     ];
 });
