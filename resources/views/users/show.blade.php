99ce553e467199041d900dceba9cf9dd853ddd03
Ursula Messick <ursula.messick@usaepay.com>
2017-03-27 20:00:13 -0700

    fix link

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,22 +2,21 @@
 
 @section('content')
 	<div class="app-header">
-      <div class="container">
-        <h1 class="app-title">{{ $user->name }}</h1>
-        <p class="lead app-description">{{ $user->tagline }}</p>
-      </div>
-    </div>
+		<div class="container">
+			<h1 class="app-title">{{ $user->name }}</h1>
+			<p class="lead app-description">{{ $user->tagline }}</p>
+		</div>
+	</div>
 
-    @if (session('status'))
-    <div class="alert alert-warning" role="alert">
-      {{ session('status') }}
-    </div>
-    @endif
+	@if (session('status'))
+		<div class="alert alert-warning" role="alert">
+			{{ session('status') }}
+		</div>
+	@endif
 
 	<div class="col-sm-12 app-main">
-	@foreach($spaces as $space) 
-        {{ $space->title }} <a href="{{ route('space', ['id'=>$space->id])}}"><br>
-    @endforeach
+		@foreach($spaces as $space) 
+			<a href="{{ route('space', ['id'=>$space->id])}}">{{ $space->title }}</a><br>
+		@endforeach
 	</div><!-- /.app-main -->
-
 @endsection
